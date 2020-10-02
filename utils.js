138a6fa18d7634f6e22cc250be68d1acd3e932f9
Static Variable James <staticvariablejames@gmail.com>
2020-10-01 22:33:38 -0500

    Add date mocking.

---
+++ utils.js
@@ -0,0 +1,7 @@
+var Util = {};
+Util.currentDate = Date.now();
+Util.mockedDate = 1598313600000; // 2020-08-25 00:00:00 UTC
+Util.dateNow = Date.now;
+
+// Allows the clock to tick starting from Util.mockedDate.
+Date.now = () => Util.dateNow() - Util.currentDate + Util.mockedDate;
